d843a213b422707631970013bb900c98dd6b8418
mrthx <marthaps007@gmail.com>
2022-08-25 11:11:00 -0500

    creación clase Saludo y clase Persona en Java

--- 4-JDKJava/pooJava/src/MiSaludo/EjemploSaludo.java
+++ 4-JDKJava/pooJava/src/MiSaludo/EjemploSaludo.java
@@ -1,3 +1,5 @@
+package MiSaludo;
+
 public class EjemploSaludo {
     public static void main(String[] args) {
         Saludo objSaludo;
@@ -7,5 +9,10 @@ public class EjemploSaludo {
         Saludo objSaludo0 = new Saludo();
         System.out.println(objSaludo.saludar0());
         System.out.println("este es otro objeto: ->" + objSaludo0.saludar0());
+    objSaludo.textoS = "Hola";
+        System.out.println("objSaludo0 = " + objSaludo);
+
+        objSaludo0.a = 4;
+        System.out.println("objSaludo0 = " + objSaludo0);
     }
 }
--- 4-JDKJava/pooJava/src/MiSaludo/Saludo.java
+++ 4-JDKJava/pooJava/src/MiSaludo/Saludo.java
@@ -1,12 +1,17 @@
+package MiSaludo;
+
 public class Saludo {
+    String textoS = "hola mundo java op";
+            int a = 3;
     void saludar() {  //la acción de la clase siempre es un verbo
         System.out.println("Hola Java OOP");
     }
-        String saludar0(){
+        String saludar0(){ //este es el método
             String textoS = "hola mundo java op";
             return textoS;
         }
 
+    }
 
 
-}
+
